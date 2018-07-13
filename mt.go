a9c5495c40ee70a3aee89bc9f0dc27566d76ca64
atsuya <37957375+atsuya0@users.noreply.github.com>
2018-07-13 22:47:29 +0900

    ゴミ箱へ移動させる関数の作成

--- mt.go
+++ mt.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"os"
+	"path"
+	"strings"
+	"time"
 )
 
 func createTrashCan(path string) error {
@@ -15,11 +18,33 @@ func createTrashCan(path string) error {
 	return nil
 }
 
+func moveToTrashCan(files) {
+	const now string = strings.Replace(time.Now().Format("2006-01-02 15:04:05"), " ", "_", 1)
+
+	for _, file := range files {
+		if _, err := os.Stat(file); err != nil {
+			log.Fatal(err)
+		} else {
+			if err := os.Rename(file, now+"_"+path.Base(file)); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
 func main() {
-	trashCanPath := os.Getenv("HOME") + "/.Trash"
+	if len(os.Args) < 2 {
+		fmt.Println("引数が足りません")
+		os.Exit(0)
+	}
+
+	const trashCanPath string = os.Getenv("HOME") + "/.Trash"
 
 	err := createTrashCan(trashCanPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // [todo] log 種類調べる
+		os.Exit(0)     // [todo] 番号を変える
 	}
+
+	moveToTrashCan(os.Args)
 }
