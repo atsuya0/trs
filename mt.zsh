cc0eba102fb3789a54fe095af6a6b66dc7a44e12
atsuya <37957375+atsuya0@users.noreply.github.com>
2018-07-22 11:26:41 +0900

    Confirm the existence of fzf.

--- mt.zsh
+++ mt.zsh
@@ -4,24 +4,23 @@ function mt() {
   typeset -r trash="${HOME}/.Trash"
   local fzf_option="--preview-window='right:hidden' --bind='ctrl-v:toggle-preview'"
 
+  ! type fzf > /dev/null 2>&1 && [[ -e ${GOPATH}/bin/mt ]] && ${GOPATH}/bin/mt $@
+
   case $1 in
     'move')
-      [[ -z $2 ]] && type fzf > /dev/null 2>&1 \
-        && set 'move' $(command ls -A ./ | sed "/^${trash##*/}$/"d \
+      [[ -z $2 ]] && set 'move' $(command ls -A ./ | sed "/^${trash##*/}$/"d \
         | eval "fzf --header='move files in the current directory to the trash' \
         --preview=\"file {} | sed 's/^.*: //'; du -hs {} | cut -f1; less {}\" ${fzf_option}") \
         > /dev/null && [[ -z $2 ]] && return
     ;;
     'restore')
-      [[ -z $2 ]] && type fzf > /dev/null 2>&1 \
-        && set 'restore' $(command ls -rA ${trash} \
+      [[ -z $2 ]] && set 'restore' $(command ls -rA ${trash} \
         | eval "fzf --header='move files in the trash to the current directory' \
         --preview=\"file ${trash}/{} | sed 's/^.*: //'; du -hs ${trash}/{} | cut -f1; echo '\n'; less ${trash}/{}\" ${fzf_option}") \
         > /dev/null && [[ -z $2 ]] && return
     ;;
     'delete')
-      [[ -z $2 ]] && type fzf > /dev/null 2>&1 \
-        && set 'delete' $(command ls -rA ${trash} \
+      [[ -z $2 ]] && set 'delete' $(command ls -rA ${trash} \
         | eval "fzf --header='delete files in the trash' \
         --preview=\"file ${trash}/{} | sed 's/^.*: //'; du -hs ${trash}/{} | cut -f1; echo '\n'; less ${trash}/{}\" ${fzf_option}") \
         > /dev/null && [[ -z $2 ]] && return
@@ -29,5 +28,6 @@ function mt() {
     *)
     ;;
   esac
+
   [[ -e ${GOPATH}/bin/mt ]] && ${GOPATH}/bin/mt $@
 }
