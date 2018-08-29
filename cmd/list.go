77aeb6cd01a525b3dc2010aba5b87122a64940e7
atsuya <37957375+atsuya0@users.noreply.github.com>
2018-08-29 16:49:03 +0900

    divide files by date.

--- cmd/list.go
+++ cmd/list.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"sort"
 	"syscall"
 	"time"
@@ -12,69 +13,87 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	executable os.FileMode = 0111
+	header                 = "\x1b[4;30;47m%s\x1b[0m\n"
+	green                  = "\x1b[32m\x1b[1m%s\x1b[39m\x1b[0m\n"
+	blue                   = "\x1b[34m\x1b[1m%s\x1b[39m\x1b[0m\n"
+	cyan                   = "\x1b[36m\x1b[1m%s\x1b[39m\x1b[0m\n"
+	white                  = "\x1b[37m\x1b[0m%s\x1b[39m\x1b[0m\n"
+)
+
 type Options struct {
 	days    int
 	reverse bool
 }
 
-type Files []os.FileInfo
+type Dirs []os.FileInfo
 
-func (f Files) Len() int {
-	return len(f)
+func (d Dirs) Len() int {
+	return len(d)
 }
 
-func (f Files) Less(i, j int) bool {
-	return f[i].Sys().(*syscall.Stat_t).Ctim.Nano() <
-		f[j].Sys().(*syscall.Stat_t).Ctim.Nano()
+func (d Dirs) Less(i, j int) bool {
+	return d[i].Sys().(*syscall.Stat_t).Ctim.Nano() <
+		d[j].Sys().(*syscall.Stat_t).Ctim.Nano()
 }
 
-func (f Files) Swap(i, j int) {
-	f[i], f[j] = f[j], f[i]
+func (d Dirs) Swap(i, j int) {
+	d[i], d[j] = d[j], d[i]
 }
 
-// ゴミ箱の中のファイル一覧を表示
-func list(options *Options, out io.Writer) error {
-	path, err := getSrc()
+func printFiles(out io.Writer, path string) error {
+	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		return err
 	}
 
-	fileInfo, err := ioutil.ReadDir(path)
+	fmt.Fprintf(out, header, filepath.Base(path))
+
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Fprintf(out, blue, file.Name())
+		} else if file.Mode()&os.ModeSymlink != 0 {
+			fmt.Fprintf(out, cyan, file.Name())
+		} else if file.Mode()&executable != 0 {
+			fmt.Fprintf(out, green, file.Name())
+		} else {
+			fmt.Fprintf(out, white, file.Name())
+		}
+	}
+
+	return nil
+}
+
+// ゴミ箱の中のファイル一覧を表示
+func list(options *Options) error {
+	trashPath := getTrashPath()
+
+	dirs, err := ioutil.ReadDir(trashPath)
 	if err != nil {
 		return err
 	}
 
 	if options.reverse {
-		sort.Sort(sort.Reverse(Files(fileInfo)))
+		sort.Sort(sort.Reverse(Dirs(dirs)))
 	} else {
-		sort.Sort(Files(fileInfo))
+		sort.Sort(Dirs(dirs))
 	}
-
-	const executable os.FileMode = 0111
-	const green = "\x1b[32m\x1b[1m%s\x1b[39m\x1b[0m\n"
-	const blue = "\x1b[34m\x1b[1m%s\x1b[39m\x1b[0m\n"
-	const cyan = "\x1b[36m\x1b[1m%s\x1b[39m\x1b[0m\n"
-	const white = "\x1b[37m\x1b[0m%s\x1b[39m\x1b[0m\n"
 
 	daysAgo := time.Now().AddDate(0, 0, -options.days)
 
-	for _, info := range fileInfo {
-		internalStat, ok := info.Sys().(*syscall.Stat_t)
+	for _, dir := range dirs {
+		internalStat, ok := dir.Sys().(*syscall.Stat_t)
 		if !ok {
 			return fmt.Errorf("fileInfo.Sys(): cast error")
 		}
-
 		if options.days != 0 && internalStat.Ctim.Nano() < daysAgo.UnixNano() {
 			continue
 		}
-		if info.IsDir() {
-			fmt.Fprintf(out, blue, info.Name())
-		} else if info.Mode()&os.ModeSymlink != 0 {
-			fmt.Fprintf(out, cyan, info.Name())
-		} else if info.Mode()&executable != 0 {
-			fmt.Fprintf(out, green, info.Name())
-		} else {
-			fmt.Fprintf(out, white, info.Name())
+
+		if err =
+			printFiles(os.Stdout, filepath.Join(trashPath, dir.Name())); err != nil {
+			return err
 		}
 	}
 
@@ -88,7 +107,7 @@ func cmdList() *cobra.Command {
 		Use:   "list",
 		Short: "The list of the trash",
 		RunE: func(_ *cobra.Command, _ []string) error {
-			return list(options, os.Stdout)
+			return list(options)
 		},
 	}
 	cmd.Flags().IntVarP(&options.days, "days", "d", 0, "How many days ago")
--- cmd/move.go
+++ cmd/move.go
@@ -16,15 +16,28 @@ func removeExt(fileName string) string {
 	return path.Base(fileName[:len(fileName)-len(filepath.Ext(fileName))])
 }
 
-func addAffix(fileName string, affix string, trashPath string) string {
-	return trashPath + "/" +
+func addAffix(fileName string, affix string, destination string) string {
+	return destination + "/" +
 		strings.Replace(removeExt(fileName), " ", "", -1) +
 		affix +
 		filepath.Ext(fileName)
 }
 
+func getDestination(trashPath string) (string, error) {
+	destination := filepath.Join(trashPath, time.Now().Format("2006-01-02"))
+	if _, err := os.Stat(destination); err == nil {
+		return destination, nil
+	}
+
+	if err := os.Mkdir(destination, 0700); err != nil {
+		return "", err
+	}
+
+	return destination, nil
+}
+
 func move(_ *cobra.Command, args []string) error {
-	trashPath, err := getSrc()
+	destination, err := getDestination(getTrashPath())
 	if err != nil {
 		return err
 	}
@@ -37,7 +50,7 @@ func move(_ *cobra.Command, args []string) error {
 			continue
 		}
 
-		if err := os.Rename(fileName, addAffix(fileName, affix, trashPath)); err != nil {
+		if err := os.Rename(fileName, addAffix(fileName, affix, destination)); err != nil {
 			log.Println(err)
 		}
 	}
